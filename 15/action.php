e8a25a08fa1d44b7880cedc7f957888838487d90
givshuk <givhuk58@gmail.com>
2017-06-23 17:43:25 +0300

    Изменение согласно PSR

--- 15/action.php
+++ 15/action.php
@@ -6,13 +6,23 @@ $b = $_GET ['b'];
 
 //var_dump($_GET);
 
-if ($b==0 and $operator=='/'){
+if ($b == 0 and $operator == '/') {
     echo "Деление на ноль";
-}else {
-    if ($operator == '+') {$result=$a + $b; }
-    if ($operator == '-') {$result=$a - $b;}
-    if ($operator =='/')  {$result=$a / $b;}
-    if ($operator == '*') {$result=$a * $b;}
-    if ($operator == '%') {$result=$a % $b;}
-    echo $a .$operator .$b .'='  .$result;
+} else {
+    if ($operator == '+') {
+        $result = $a + $b;
+    }
+    if ($operator == '-') {
+        $result = $a - $b;
+    }
+    if ($operator == '/') {
+        $result = $a / $b;
+    }
+    if ($operator == '*') {
+        $result = $a * $b;
+    }
+    if ($operator == '%') {
+        $result = $a % $b;
+    }
+    echo $a . $operator . $b . '=' . $result;
 }
