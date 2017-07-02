0311fb5157ad6c29a1e5526d996406280eca747a
givshuk <givshuk58@gmail.com>
2017-07-02 23:06:18 +0300

    Удалил все отредатирвоал README

--- 14.php
+++
@@ -1,8 +0,0 @@
-<?php
-$foo = 'bar';
-$bar = 10;
-
-$foo = &$bar;
-
-echo '10 = '.$foo;
-
--- 27.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: andrey
- * Date: 21.06.17
- * Time: 13:51
- */
-//Запустить консоль в папке с файлом и  набрать : php имя файла.php
